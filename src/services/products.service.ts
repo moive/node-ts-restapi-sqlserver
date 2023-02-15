fd161611380bf24cbc0f820bbffa37250140ed05
Moises Velasquez <mvelasquezdeveloper@gmail.com>
2023-02-15 18:11:39 -0500

    fix error services api

--- src/services/products.service.ts
+++ src/services/products.service.ts
@@ -1,11 +1,16 @@
 import sql from 'mssql';
 import { getConnection, queries } from '../database';
 import type { IProduct } from '../interfaces/products.interface';
+import { ResponseError } from '../utils/custom.error';
 
 const getProductsService = async (): Promise<any> => {
-  const pool = await getConnection();
-  const result = await pool.request().query(queries.allProducts);
-  return result;
+  try {
+    const pool = await getConnection();
+    const result = await pool.request().query(queries.allProducts);
+    return result;
+  } catch (e: any) {
+    throw new ResponseError(e.message);
+  }
 };
 
 const createProductService = async ({
@@ -13,32 +18,44 @@ const createProductService = async ({
   description,
   quantity
 }: IProduct): Promise<any> => {
-  const pool = await getConnection();
-  const result = await pool
-    .request()
-    .input('name', sql.VarChar, name)
-    .input('description', sql.Text, description)
-    .input('quantity', sql.Int, quantity)
-    .query(queries.addNewProduct);
-  return result;
+  try {
+    const pool = await getConnection();
+    const result = await pool
+      .request()
+      .input('name', sql.VarChar, name)
+      .input('description', sql.Text, description)
+      .input('quantity', sql.Int, quantity)
+      .query(queries.addNewProduct);
+    return result;
+  } catch (e: any) {
+    throw new ResponseError(e.message);
+  }
 };
 
 const getProductByIdService = async (id: number): Promise<any> => {
-  const pool = await getConnection();
-  const result = await pool
-    .request()
-    .input('Id', sql.Int, id)
-    .query(queries.getProductById);
-  return result;
+  try {
+    const pool = await getConnection();
+    const result = await pool
+      .request()
+      .input('Id', sql.Int, id)
+      .query(queries.getProductById);
+    return result;
+  } catch (e: any) {
+    throw new ResponseError(e.message);
+  }
 };
 
 const deleteProductService = async (id: number): Promise<any> => {
-  const pool = await getConnection();
-  const result = await pool
-    .request()
-    .input('Id', sql.Int, id)
-    .query(queries.deleteProductById);
-  return result;
+  try {
+    const pool = await getConnection();
+    const result = await pool
+      .request()
+      .input('Id', sql.Int, id)
+      .query(queries.deleteProductById);
+    return result;
+  } catch (e: any) {
+    throw new ResponseError(e.message);
+  }
 };
 
 export {
